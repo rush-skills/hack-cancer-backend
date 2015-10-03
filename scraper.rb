c2dadba6935dd1862369f7cdd0e41a3576391dbc
Ankur Singh <ankur13019@iiitd.ac.in>
2015-10-03 21:16:46 +0530

    added flags and basic scoring

--- scraper.rb
+++ scraper.rb
@@ -17,7 +17,7 @@ end
 DOCACHING = false
 
 get '/' do
-  return "send me url please"
+  return "send me url plz"
 end
 
 get '/*' do
@@ -39,19 +39,29 @@ get '/*' do
 end
 
 def compute(url)
-  is_gov = check_gov(url)
-  answer = JSON.parse(open(diff_it(url)).read)
-  a = analyze_diff answer
-  a[:is_gov] = is_gov
-  {answer: a}
+  begin
+    is_gov = check_gov(url)
+    answer = JSON.parse(open(diff_it(url)).read)
+    a = analyze_diff answer
+    a[:is_gov] = is_gov
+    flags = compute_flags a
+    score = compute_score flags
+    {response: {answer: a,flags: flags, score: score}}
+  rescue
+    {response: {score: -1}}
+  end
 end
 
 
 def check_gov(url)
   # puts url.split("/")[1]
-  domain = url.split("/")[1].split('.')
-  gov_domains = ["gov","ac","edu"]
-  gov_domains.include? domain[-1] or gov_domains.include? domain[-2]
+  begin
+    domain = url.split("/")[1].split('.')
+    gov_domains = ["gov","ac","edu"]
+    gov_domains.include? domain[-1] or gov_domains.include? domain[-2]
+  rescue
+    false
+  end
 end
 
 def diff_it(url)
@@ -65,9 +75,15 @@ def analyze_diff(data)
   begin
     object = data['objects'][0]
     fields = {sentiment: object['sentiment'], text: object['text'], date: object['date'], estimated_date: object['estimatedDate'],title: object['title'], title_size: object['title'].split(' ').count, author: object['author']}
-    score = compute_diff_score(fields)
-    fields[:days_old] = score.to_i
-    fields[:average_letters_per_word] = count_words(object['text'])
+    fields.default = nil
+    if fields[:date]
+      score = compute_diff_score(fields)
+      fields[:days_old] = score.to_i
+    end
+    if fields[:text]
+      fields[:average_letters_per_word] = count_words(object['text'])[0]
+      fields[:total_words] = count_words(object['text'])[1]
+    end
     return fields
   rescue
 
@@ -76,10 +92,33 @@ end
 
 def count_words(text)
   counter = WordsCounted.count(text)
-  counter.average_chars_per_word
+  [counter.average_chars_per_word,counter.word_count]
 end
 
 def compute_diff_score(fields)
-  # puts Date.parse(fields[:date]) - Date.today
   Date.today - Date.parse(fields[:date])
+end
+
+def compute_flags(data)
+  flags = Hash.new(false)
+  flags[:words_flag] = data[:average_letters_per_word] > 7 if data[:average_letters_per_word]
+  flags[:title_flag] = data[:title_size] > 25 if data[:title_size]
+  flags[:sentiment_flag] = data[:sentiment] < -0.5 if data[:sentiment]
+  flags[:text_size_flag] = data[:total_words] < 100 or data[:total_words] > 5000 if data[:total_words]
+  flags[:old_flag] = data[:days_old] > 365*3 if data[:days_old]
+  flags[:super_old_flag] = data[:days_old] > 365*5 if data[:days_old]
+  flags[:gov_flag] = data[:is_gov]
+  flags
+end
+
+def compute_score(data)
+  base = 100
+  base += 100 if data[:gov_flag]
+  base -= 50 if data[:sentiment_flag]
+  base -= 50 if data[:title_flag]
+  base -= 50 if data[:words_flag]
+  base -= 50 if data[:age_flag]
+  base -= 50 if data[:super_old_flag]
+  base -= 50 if data[:text_size_flag]
+  base <= 0? 1:0
 end
